52cc174479d3125e7fa222a644d90717fdd6d00b
JXG052 <jxg0521@gmail.com>
2023-05-26 18:02:16 +0100

    Updated for Davyhulme Trophy and Singles Stableford

--- src/components/MoneyListTable.js
+++ src/components/MoneyListTable.js
@@ -25,48 +25,48 @@ export default function MoneyListTable({ info }) {
                             <TableCell>{golfer.points} points</TableCell>
                         </TableRow>
                     ))} */}
-                    <TableRow key="0">
-                        <TableCell>{data[0].ranking}</TableCell>
-                        <TableCell>{data[0].name}</TableCell>
-                        <TableCell>{data[0].rounds}</TableCell>
-                        <TableCell>{data[0].bestFinish}</TableCell>
-                        <TableCell>{data[0].points} points</TableCell>
-                    </TableRow>
-                    <TableRow key="0">
+                    <TableRow key="0"> {/* Jonny */}
                         <TableCell>{data[1].ranking}</TableCell>
                         <TableCell>{data[1].name}</TableCell>
                         <TableCell>{data[1].rounds}</TableCell>
                         <TableCell>{data[1].bestFinish}</TableCell>
                         <TableCell>{data[1].points} points</TableCell>
                     </TableRow>
+                    <TableRow key="0"> {/* Duckett */}
+                        <TableCell>{data[0].ranking}</TableCell>
+                        <TableCell>{data[0].name}</TableCell>
+                        <TableCell>{data[0].rounds}</TableCell>
+                        <TableCell>{data[0].bestFinish}</TableCell>
+                        <TableCell>{data[0].points} points</TableCell>
+                    </TableRow> 
 
-                    <TableRow key="0">
+                    <TableRow key="0"> {/* Matty */}
                         <TableCell>{data[3].ranking}</TableCell>
                         <TableCell>{data[3].name}</TableCell>
                         <TableCell>{data[3].rounds}</TableCell>
                         <TableCell>{data[3].bestFinish}</TableCell>
                         <TableCell>{data[3].points} points</TableCell>
                     </TableRow>
-                    <TableRow key="0">
-                        <TableCell>{data[2].ranking}</TableCell>
-                        <TableCell>{data[2].name}</TableCell>
-                        <TableCell>{data[2].rounds}</TableCell>
-                        <TableCell>{data[2].bestFinish}</TableCell>
-                        <TableCell>{data[2].points} points</TableCell>
-                    </TableRow>
-                    <TableRow key="0">
+                    <TableRow key="0"> {/* Sam */}
                         <TableCell>{data[5].ranking}</TableCell>
                         <TableCell>{data[5].name}</TableCell>
                         <TableCell>{data[5].rounds}</TableCell>
                         <TableCell>{data[5].bestFinish}</TableCell>
                         <TableCell>{data[5].points} points</TableCell>
                     </TableRow>
-                    <TableRow key="0">
+                    <TableRow key="0"> {/* Phil */}
                         <TableCell>{data[4].ranking}</TableCell>
                         <TableCell>{data[4].name}</TableCell>
                         <TableCell>{data[4].rounds}</TableCell>
                         <TableCell>{data[4].bestFinish}</TableCell>
                         <TableCell>{data[4].points} points</TableCell>
+                    </TableRow>
+                    <TableRow key="0"> {/* Lewis */}
+                        <TableCell>{data[2].ranking}</TableCell>
+                        <TableCell>{data[2].name}</TableCell>
+                        <TableCell>{data[2].rounds}</TableCell>
+                        <TableCell>{data[2].bestFinish}</TableCell>
+                        <TableCell>{data[2].points} points</TableCell>
                     </TableRow>
 
                 </TableBody>
